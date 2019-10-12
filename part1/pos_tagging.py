0abe9c1974b86f7963c789b3f9190278ea5f7392
wuziang <wuziang0104@gmail.com>
2019-10-12 15:02:31 +0800

    Randomly Select 5 Reviews for POS Tagging

--- part1/pos_tagging.py
+++ part1/pos_tagging.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
 import numpy as np
 import pandas as pd
 
@@ -12,21 +6,11 @@ import nltk
 import os.path
 
 
-# In[2]:
-
-
 review_path = '../data/reviewSelected100.json'
 os.path.exists(review_path)
 
-
-# In[3]:
-
-
 review_df = pd.read_json(review_path, lines=True)
 review_df.head()
-
-
-# In[38]:
 
 
 review_random_idx = np.array(np.random.rand(5)*len(review_df), dtype=np.int32)
@@ -37,9 +21,6 @@ review_random_df = review_random_df.reset_index()
 review_random_df
 
 
-# In[39]:
-
-
 # nltk.download('punkt')
 # nltk.download('averaged_perceptron_tagger')
 
@@ -47,8 +28,5 @@ review_random_df['tokenize'] = review_random_df['text'].apply(nltk.word_tokenize
 review_random_df['pos_tag'] = review_random_df['tokenize'].apply(nltk.pos_tag)
 
 
-# In[48]:
-
-
-review_random_df.to_json(r'../data/reviewTagging100.json', orient='records', lines=True)
+review_random_df.to_json(r'../data/reviewTagging5.json', orient='records', lines=True)
 
